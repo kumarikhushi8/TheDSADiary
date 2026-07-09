fbdfa7755967e1d1d2621f22d265fe8fa0500a24
Kumari Khushi <flourishingflaws06@gmail.com>
2026-07-09 08:49:26 +0530

    Solve LeetCode 1047: Remove All Adjacent Duplicates In String
    Implemented a solution to remove all adjacent duplicates from a string using a stack.

---
+++ Stack/Remove All Adjacent Duplicates In String.cpp
@@ -0,0 +1,28 @@
+/*
+Problem: Remove All Adjacent Duplicates In String
+LeetCode: 1047
+Approach: Stack
+Time Complexity: O(n)
+Space Complexity: O(n)
+*/
+
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        stack<char>st;
+        for(int i=0;i<s.size();i++){
+            if(!st.empty() && st.top()==s[i]){
+                st.pop();
+            }else{
+                st.push(s[i]);
+            }
+        }
+        string ans="";
+        while(st.size()!=0){
+            ans+=st.top();
+            st.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
