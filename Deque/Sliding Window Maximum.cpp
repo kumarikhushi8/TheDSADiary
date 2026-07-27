75a610f63ddd61fef76541e597daac7700fb286f
Kumari Khushi <flourishingflaws06@gmail.com>
2026-07-27 13:58:39 +0530

    Solve LeetCode 239: Sliding Window Maximum using Monotonic Deque
    Implement sliding window maximum using a monotonic deque.

---
+++ Deque/Sliding Window Maximum.cpp
@@ -0,0 +1,24 @@
+/*
+Problem: Sliding Window Maximum
+LeetCode: 239
+Approach: Monotonic Deque
+Time Complexity: O(n)
+Space Complexity: O(k)
+*/
+
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        vector<int>res;
+        deque<int>dq;
+        for(int i=0;i<nums.size();i++){
+            while(!dq.empty() && dq.back()<nums[i]){
+                dq.pop_back();
+            }
+            dq.push_back(nums[i]);
+            if(i>=k-1) res.push_back(dq.front());
+            if(i-k+1>=0 && dq.front()==nums[i-k+1]) dq.pop_front();
+        }
+        return res;
+    }
+};
