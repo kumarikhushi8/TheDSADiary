bf22fa8dad99c33347143b2dfa9edf685cfa8826
Kumari Khushi <flourishingflaws06@gmail.com>
2026-07-28 13:45:40 +0530

    Solve LeetCode 206: Reverse Linked List using iterative approach
    Added a new file for reversing a linked list using an iterative approach.

---
+++ Linked List/Reverse Linked List.cpp
@@ -0,0 +1,32 @@
+/*
+Problem: Reverse Linked List
+LeetCode: 206
+Approach: Iterative Three-Pointer Technique
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev=nullptr;
+        ListNode* curr=head;
+        while(curr!=nullptr) {
+            ListNode* next=curr->next;  
+            curr->next=prev; 
+            prev=curr;
+            curr=next; 
+        }
+        return prev; 
+    }
+};
