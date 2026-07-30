0d48ac9689ab0e3436f5adfc62d7c639a2bb2f26
Kumari Khushi <flourishingflaws06@gmail.com>
2026-07-30 10:07:35 +0530

    Solve LeetCode 234: Palindrome Linked List using O(1) space
    Implemented a solution to check if a linked list is a palindrome using the slow and fast pointer technique, followed by reversing the second half of the list and comparing it with the first half.

--- Linked List/Palindrome Linked List.cpp
+++ Linked List/Palindrome Linked List.cpp
@@ -1,6 +1,59 @@
 /*
 Problem: Palindrome Linked List
 LeetCode: 234
+Approach:
+1. Find the middle using slow and fast pointers.
+2. Reverse the second half.
+3. Compare both halves.
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        ListNode* slow=head;
+        ListNode* fast=head;
+        while(fast && fast->next){
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        ListNode* secondHalf=slow;
+        ListNode* firstHalf=head;
+        ListNode* prev=nullptr;
+        while(slow!=nullptr){
+            ListNode* next=slow->next;
+            slow->next=prev;
+            prev=slow;
+            slow=next;
+        }
+        secondHalf=prev;
+        while(secondHalf){
+            if(firstHalf->val!=secondHalf->val) return false;
+            firstHalf=firstHalf->next;
+            secondHalf=secondHalf->next;
+        }
+        return true;
+    }
+};
+
+
+
+
+
+/*
+Problem: Palindrome Linked List
+LeetCode: 234
 Approach: Store linked list values in a vector and check palindrome using two pointers.
 Time Complexity: O(n)
 Space Complexity: O(n)
