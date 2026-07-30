a91aa24d68d70d69822c424a6273463691fa2b79
Kumari Khushi <flourishingflaws06@gmail.com>
2026-07-30 09:50:14 +0530

    Solve LeetCode 234: Palindrome Linked List using vector
    Added a comment block explaining the problem, approach, and complexities for the Palindrome Linked List solution.

---
+++ Linked List/Palindrome Linked List.cpp
@@ -0,0 +1,39 @@
+/*
+Problem: Palindrome Linked List
+LeetCode: 234
+Approach: Store linked list values in a vector and check palindrome using two pointers.
+Time Complexity: O(n)
+Space Complexity: O(n)
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        vector<int>arr;
+        ListNode* curr=head;
+        while(curr){
+            arr.push_back(curr->val);
+            curr=curr->next;
+        }
+        int i=0;
+        int j=arr.size()-1;
+        while(i<j){
+            if(arr[i]!=arr[j]){
+               return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+};
